252f85b896a9cc52ece3d314ec0d1a8332e9dbc2
Nico Varley <anicovarley@gmail.com>
2026-04-05 22:28:15 +0000

    Updated the value base class to expose the value property.

--- code/core/value.ts
+++ code/core/value.ts
@@ -3,9 +3,9 @@ import type { Mutability } from "./mutability/mutability.ts";
 export abstract class Value {
     public abstract readonly type: string;
     public abstract readonly mutability: Mutability;
-    protected readonly value: number;
+    public readonly value: number;
 
-    protected constructor(value: number) {
-        this.value = value;
+    protected constructor(value?: number) {
+        this.value = value ?? 0;
     }
 }
