bce26624c6d68caaa2bcea423220c3764bd22ca9
Nico Varley <anicovarley@gmail.com>
2026-04-05 21:40:33 +0000

    Changed imports to .ts import style for consistency.

--- code/measurements/time/base/immutable_time_unit.ts
+++ code/measurements/time/base/immutable_time_unit.ts
@@ -1,6 +1,6 @@
-import { Time_Unit } from "./time_unit";
-import type { Immutable } from "../../../core/mutability/immutable";
-import type { Mutable_Time_Unit } from "./mutable_time_unit";
+import { Time_Unit } from "./time_unit.ts";
+import type { Immutable } from "../../../core/mutability/immutable.ts";
+import type { Mutable_Time_Unit } from "./mutable_time_unit.ts";
 
 export abstract class Immutable_Time_Unit extends Time_Unit implements Immutable<Mutable_Time_Unit> {
     public readonly mutability = "immutable" as const;
